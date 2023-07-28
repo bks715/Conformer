31a52d801c3e6311d92a42092868d8423076fe4a
bradensmith <braden@appsmiths.io>
2023-07-28 17:42:11 -0500

    Added ability to handle optionals when adding member vars

--- Sources/ConformerMacros/ConformerMacro.swift
+++ Sources/ConformerMacros/ConformerMacro.swift
@@ -31,94 +31,6 @@ public struct SupamodeledMacro: ConformanceMacro, MemberMacro {
     }
     
     public static func expansion(of node: AttributeSyntax, providingMembersOf declaration: some DeclGroupSyntax, in context: some MacroExpansionContext) throws -> [DeclSyntax] {
-        /*
-        StructDeclSyntax
-        ├─attributes: AttributeListSyntax
-        │ ╰─[0]: AttributeSyntax
-        │   ├─atSignToken: atSign
-        │   ╰─attributeName: SimpleTypeIdentifierSyntax
-        │     ╰─name: identifier("Supamodeled")
-        ├─structKeyword: keyword(SwiftSyntax.Keyword.struct)
-        ├─identifier: identifier("DemoTable")
-        ╰─memberBlock: MemberDeclBlockSyntax
-          ├─leftBrace: leftBrace
-          ├─members: MemberDeclListSyntax
-          │ ├─[0]: MemberDeclListItemSyntax
-          │ │ ╰─decl: VariableDeclSyntax
-          │ │   ├─bindingKeyword: keyword(SwiftSyntax.Keyword.var)
-          │ │   ╰─bindings: PatternBindingListSyntax
-          │ │     ╰─[0]: PatternBindingSyntax
-          │ │       ├─pattern: IdentifierPatternSyntax
-          │ │       │ ╰─identifier: identifier("tableName")
-          │ │       ├─typeAnnotation: TypeAnnotationSyntax
-          │ │       │ ├─colon: colon
-          │ │       │ ╰─type: SimpleTypeIdentifierSyntax
-          │ │       │   ╰─name: identifier("String")
-          │ │       ╰─initializer: InitializerClauseSyntax
-          │ │         ├─equal: equal
-          │ │         ╰─value: StringLiteralExprSyntax
-          │ │           ├─openQuote: stringQuote
-          │ │           ├─segments: StringLiteralSegmentsSyntax
-          │ │           │ ╰─[0]: StringSegmentSyntax
-          │ │           │   ╰─content: stringSegment("demo_table")
-          │ │           ╰─closeQuote: stringQuote
-          │ ├─[1]: MemberDeclListItemSyntax
-          │ │ ╰─decl: VariableDeclSyntax
-          │ │   ├─bindingKeyword: keyword(SwiftSyntax.Keyword.var)
-          │ │   ╰─bindings: PatternBindingListSyntax
-          │ │     ╰─[0]: PatternBindingSyntax
-          │ │       ├─pattern: IdentifierPatternSyntax
-          │ │       │ ╰─identifier: identifier("columns")
-          │ │       ├─typeAnnotation: TypeAnnotationSyntax
-          │ │       │ ├─colon: colon
-          │ │       │ ╰─type: ArrayTypeSyntax
-          │ │       │   ├─leftSquareBracket: leftSquareBracket
-          │ │       │   ├─elementType: ConstrainedSugarTypeSyntax
-          │ │       │   │ ├─someOrAnySpecifier: keyword(SwiftSyntax.Keyword.any)
-          │ │       │   │ ╰─baseType: SimpleTypeIdentifierSyntax
-          │ │       │   │   ╰─name: identifier("TableColumnProtocol")
-          │ │       │   ╰─rightSquareBracket: rightSquareBracket
-          │ │       ╰─initializer: InitializerClauseSyntax
-          │ │         ├─equal: equal
-          │ │         ╰─value: FunctionCallExprSyntax
-          │ │           ├─calledExpression: ClosureExprSyntax
-          │ │           │ ├─leftBrace: leftBrace
-          │ │           │ ├─statements: CodeBlockItemListSyntax
-          │ │           │ │ ╰─[0]: CodeBlockItemSyntax
-          │ │           │ │   ╰─item: ReturnStmtSyntax
-          │ │           │ │     ├─returnKeyword: keyword(SwiftSyntax.Keyword.return)
-          │ │           │ │     ╰─expression: ArrayExprSyntax
-          │ │           │ │       ├─leftSquare: leftSquareBracket
-          │ │           │ │       ├─elements: ArrayElementListSyntax
-          │ │           │ │       │ ├─[0]: ArrayElementSyntax
-          │ │           │ │       │ │ ├─expression: FunctionCallExprSyntax
-          │ │           │ │       │ │ │ ├─calledExpression: IdentifierExprSyntax
-          │ │           │ │       │ │ │ │ ╰─identifier: identifier("TableColumn")
-          │ │           │ │       │ │ │ ├─leftParen: leftParen
-          │ │           │ │       │ │ │ ├─argumentList: TupleExprElementListSyntax
-          │ │           │ │       │ │ │ │ ├─[0]: TupleExprElementSyntax
-          │ │           │ │       │ │ │ │ │ ├─label: identifier("name")
-          │ │           │ │       │ │ │ │ │ ├─colon: colon
-          │ │           │ │       │ │ │ │ │ ├─expression: StringLiteralExprSyntax
-          │ │           │ │       │ │ │ │ │ │ ├─openQuote: stringQuote
-          │ │           │ │       │ │ │ │ │ │ ├─segments: StringLiteralSegmentsSyntax
-          │ │           │ │       │ │ │ │ │ │ │ ╰─[0]: StringSegmentSyntax
-          │ │           │ │       │ │ │ │ │ │ │   ╰─content: stringSegment("demoTableId")
-          │ │           │ │       │ │ │ │ │ │ ╰─closeQuote: stringQuote
-          │ │           │ │       │ │ │ │ │ ╰─trailingComma: comma
-          │ │           │ │       │ │ │ │ ╰─[1]: TupleExprElementSyntax
-          │ │           │ │       │ │ │ │   ├─label: identifier("valueType")
-          │ │           │ │       │ │ │ │   ├─colon: colon
-          │ │           │ │       │ │ │ │   ╰─expression: MemberAccessExprSyntax
-          │ │           │ │       │ │ │ │     ├─base: IdentifierExprSyntax
-          │ │           │ │       │ │ │ │     │ ╰─identifier: identifier("String")
-          │ │           │ │       │ │ │ │     ├─dot: period
-          │ │           │ │       │ │ │ │     ╰─name: keyword(SwiftSyntax.Keyword.self)
-          │ │           │ │       │ │ │ ╰─rightParen: rightParen
-          │ │           │ │       │ │ ╰─trailingComma: comma
-          │ │           │ │       │ ├─[1]: ArrayElementSyntax
-        [
-            */
             let structDecl = declaration.cast(StructDeclSyntax.self)
             
         guard let columnMembers = structDecl.memberBlock.members.first(where: {$0.decl.as(VariableDeclSyntax.self)?.bindings.first?.pattern.as(IdentifierPatternSyntax.self)?.identifier.text == "columns" }),
@@ -133,13 +45,14 @@ public struct SupamodeledMacro: ConformanceMacro, MemberMacro {
             else { return ""}
             if let value = argumentList.last?.expression.as(MemberAccessExprSyntax.self)?.base?.as(IdentifierExprSyntax.self)?.identifier.text{
                 return "var \(name): \(value)"
+            }else if let value = argumentList.last?.expression.as(MemberAccessExprSyntax.self)?.base?.as(OptionalChainingExprSyntax.self)?.expression.as(IdentifierExprSyntax.self)?.identifier.text{
+                return "var \(name): \(value)"
             }
             return ""
         }
         return [
                 """
                 \(raw: variables.joined(separator: "\n"))
-                \(raw: structDecl.debugDescription)
                 """
         ]
     }
